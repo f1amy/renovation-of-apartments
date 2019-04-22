ecb68841be29fa6b5c231dbd3aabc45f9bdbc958
Flamy <27062841+f1amy@users.noreply.github.com>
2019-04-22 20:22:10 +0500

    update db.php, switch from cli to fpm php, tweak docker-compose.yml

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=renovation_of_apartments',
-    'username' => 'root',
+    'username' => 'webapp_user',
     'password' => '157266',
     'charset' => 'utf8',
 
